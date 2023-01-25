1438bffd2a7b8adff05c9e06934fa2dad95cdb46
https://github.com/iamblashch <bblashchuk08@gmail.com>
2023-01-25 17:34:13 +0200

    finish

--- src/js/fetch-image.js
+++ src/js/fetch-image.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-export { fetchImages };
+export { fetchImage };
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '32954054-8c97559c3455e9bff63f41603';
 
-async function fetchImages(query, page, perPage) {
+async function fetchImage(query, page, perPage) {
   const response = await axios.get(
     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
   );
